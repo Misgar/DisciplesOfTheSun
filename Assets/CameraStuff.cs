12a4eefa881412e414f279468bb9ec212622d2d9
saobernardo <bernardo.pinheiro@hotmail.com>
2019-05-31 18:44:49 -0300

    Repositioned the character in the Tomb scene and created a script for the camera to remove and lock the mouse cursor

---
+++ Assets/CameraStuff.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.AccessControl;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStuff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
