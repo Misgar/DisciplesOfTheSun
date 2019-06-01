2f628e55bb597d105ddd403888d83cde47f1a465
saobernardo <bernardo.pinheiro@hotmail.com>
2019-06-01 12:03:46 -0300

    Commentes dome code and remove a pointless song file

--- Assets/CameraStuff.cs
+++ Assets/CameraStuff.cs
@@ -1,15 +1,21 @@
-﻿using System.Security.AccessControl;
-using System.Reflection;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Security.AccessControl;
 using UnityEngine;
 
-public class CameraStuff : MonoBehaviour
-{
+public class CameraStuff : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+    /* void Update () {
+        if (Canvas_Pause.IsActive ()) {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        } else {
+            if (Canvas_pause.IsActive ()) {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = True;
+            }
+        }
+    }*/
 }
